b74452d53beadb18db650cafaea542624ffd59ed
MaxiJunkie <Slavik123>
2017-04-25 00:27:38 +0300

    Initial Commit

---
+++ MustApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MustApp
+//
+//  Created by Максим Стегниенко on 25.04.17.
+//  Copyright © 2017 Максим Стегниенко. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
